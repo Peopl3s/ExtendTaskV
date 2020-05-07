8e09fd8df7544066b46b9a7f0ed3c73b3ca44d06
Peopl3s <nikposd@mail.ru>
2020-05-07 13:53:31 +0300

    Added tabs and linespacing

--- popup.js
+++ popup.js
@@ -8,38 +8,34 @@ function getInfo(){
 };
 	
 function getWeekNumber(){
-	let d = new Date, dt = d.getTime (); d.setMonth (8, 2);
-	if (d.getTime () > dt) d.setFullYear (d.getFullYear () - 1);
-	let n = Math.floor ((dt - d.getTime ()) / 8.64e7 / 7);
-	//console.log(n);
-	let week = ((n / 2 == 0) || (n == 0 )) ? 'Знаменательная неделя': 'Числительная неделя';
+	let date = new Date, dTime = date.getTime (); date.setMonth (8, 2);
+	if (date.getTime () > dTime) date.setFullYear (date.getFullYear () - 1);
+	let n = Math.floor ((dTime - date.getTime ()) / 8.64e7 / 7);
+	let week = ((n / 2 == 0) || (n == 0 )) ? 'Г‡Г­Г Г¬ГҐГ­Г ГІГҐГ«ГјГ­Г Гї Г­ГҐГ¤ГҐГ«Гї': 'Г—ГЁГ±Г«ГЁГІГҐГ«ГјГ­Г Гї Г­ГҐГ¤ГҐГ«Гї';
 	$('#info').html('<p>'+$('#info').html() +'<br>'+ week + '</p>')
 };
 
 function loadChromeStorage(){
-	
 	chrome.storage.sync.get('index', function(res) {
-    if (res.index)
-	{
-		this.divIndex = res.index;
-	}
-  });
-		  
-chrome.storage.sync.get('data', function(res) {
-    if (res.data)
-	{
-		this.dataA = res.data;
-		divs = res.data.split('`');
-		for(str of res.data.split('`'))
-		{
-			$("#content").append(str);
+		if (res.index){
+			this.divIndex = res.index;
 		}
-	} 
-  });
+	});
+	
+	chrome.storage.sync.get('data', function(res) {
+		if (res.data)
+		{
+			this.dataA = res.data;
+			divs = res.data.split('`');
+			for(str of res.data.split('`'))
+			{
+				$("#content").append(str);
+			}
+		} 
+	});
 };
 
 function copyOnBuffer(e){
-	//console.log('test',$(this).text());
 	var $temp = $("<input>");
 	$("body").append($temp);
 	$temp.val($(this).text()).select();
@@ -48,117 +44,101 @@ function copyOnBuffer(e){
 };
 
 function createTask(e){
-        e.preventDefault();
-		
-		let task = prompt('‚ведите задачу в формате: «адача/год.мес¤ц.число/часы.минуты')
-		let headline = '<div id=\"task'+ divIndex + '\">'+task+' #'+divIndex+'</div>';     //!!!!!! <hr></div>
-		divs[divIndex] = headline;  // !!!!!
-		$("#content").append(headline);		
-		divIndex += 1;
-		dataA = divs.join('`');  
-		
-		chrome.storage.sync.set({index: divIndex}, function() {
-      console.log('Index is', divIndex);
-    });
-		chrome.storage.sync.set({data: dataA}, function() {
-      console.log('data is', dataA);
-    });	
+	e.preventDefault();
+	let task = prompt('вЂљГўГҐГ¤ГЁГІГҐ Г§Г Г¤Г Г·Гі Гў ГґГ®Г°Г¬Г ГІГҐ: В«Г Г¤Г Г·Г /ГЈГ®Г¤.Г¬ГҐГ±В¤Г¶.Г·ГЁГ±Г«Г®/Г·Г Г±Г».Г¬ГЁГ­ГіГІГ»')
+	let headline = '<div id=\"task'+ divIndex + '\">'+task+' #'+divIndex+'</div>';    
+	divs[divIndex] = headline; 
+	$("#content").append(headline);		
+	divIndex += 1;
+	dataA = divs.join('`');  
+	
+	chrome.storage.sync.set({index: divIndex}, function() {
+		console.log('Index is', divIndex);
+	});
+	chrome.storage.sync.set({data: dataA}, function() {
+		console.log('data is', dataA);
+	});	
 };
 	
 function removeTask(e){
-        e.preventDefault();
-		
-		let task = prompt('‚ведите номер задачи:')
-		$('#task'+task).remove();
-		delete divs[task]
-		dataA = divs.join('`');
-		
-		chrome.storage.sync.set({data: dataA}, function() {
-      console.log('data is', dataA);
-    });
+	e.preventDefault();
+	let task = prompt('вЂљГўГҐГ¤ГЁГІГҐ Г­Г®Г¬ГҐГ° Г§Г Г¤Г Г·ГЁ:')
+	$('#task'+task).remove();
+	delete divs[task]
+	dataA = divs.join('`');
+	chrome.storage.sync.set({data: dataA}, function() {
+		console.log('data is', dataA);
+	});
 };	
 
 function clearTaskSheldue(e){
-        e.preventDefault();
-		
-		divIndex = 1;
-		dataA = '';
-		divs = [];
-		$("#content").empty();
-		
-		chrome.storage.sync.set({index: divIndex}, function() {
-      console.log('data is', 1);
-    });
-		chrome.storage.sync.set({data: dataA}, function() {
-      console.log('data is', '');});
+	e.preventDefault();
+	divIndex = 1;
+	dataA = '';
+	divs = [];
+	$("#content").empty();
+	
+	chrome.storage.sync.set({index: divIndex}, function() {
+		console.log('data is', 1);
+	});
+	chrome.storage.sync.set({data: dataA}, function() {
+		console.log('data is', '');
+	});
 };
 
-
 function setTimerOnTask(e){
-        e.preventDefault();
+	e.preventDefault();
+	let numberTask = prompt('Г‚Г»ГЎГҐГ°ГҐГІГҐ Г­Г®Г¬ГҐГ° Г§Г Г¤Г Г·ГЁ, Г­Г  ГЄГ®ГІГ®Г°ГіГѕ ГЎГіГ¤ГҐГІ ГіГ±ГІГ Г­Г®ГўГ«ГҐГ­ ГІГ Г©Г¬ГҐГ°')
+	if ( $('#task'+numberTask).text() !=''){
+		let d_tm = prompt('Г‚ГўГҐГ¤ГЁГІГҐ Г¤Г ГІГі ГЁ ГўГ°ГҐГ¬Гї Гў ГґГ®Г°Г¬Г ГІГҐ: ГЈГ®Г¤/Г¬ГҐГ±ГїГ¶/Г·ГЁГ±Г«Г®/Г·Г Г±/Г¬ГЁГ­ГіГІГ»')
+		let now = new Date();
+		let endDate = new Date(d_tm.split('/')[0], d_tm.split('/')[1]-1, 
+				       d_tm.split('/')[2], d_tm.split('/')[3],d_tm.split('/')[4]);
+		sec = ((endDate.getTime() - now.getTime()) / 1000);
+		let msg = $('#task' + numberTask).text();
+		let newWin = window.open("timer.html", "Timer", "width=200,height=200");
 		
-		let numberTask = prompt('Выберете номер задачи, на которую будет установлен таймер')
-		
-		if ( $('#task'+numberTask).text() !=''){
-			let d_tm = prompt('Введите дату и время в формате: год/месяц/число/час/минуты')
-			//console.log(d_tm);
-			let now = new Date();
-			let endDate = new Date(d_tm.split('/')[0], d_tm.split('/')[1]-1, 
-			                       d_tm.split('/')[2], d_tm.split('/')[3],d_tm.split('/')[4]);
-			//console.log(now)
-			//console.log(endDate)
-			//console.log(((endDate.getTime() - now.getTime()) / 1000));
-			sec = ((endDate.getTime() - now.getTime()) / 1000);
-			let msg = $('#task' + numberTask).text();
-			let newWin = window.open("timer.html", "Timer", "width=200,height=200");
-			newWin.onload= function(){
-				newWin.$("body").append(`<center><div id="secondLeft"><p><b>${sec}</b></p></div></center>`);
-				newWin.$("body").append(`<center><div id="message"><p><b>${msg}</b></p></div><center>`);
-			};
-		} else {
-			alert('Нет такой задачи')
-		}
+		newWin.onload= function(){
+			newWin.$("body").append(`<center><div id="secondLeft"><p><b>${sec}</b></p></div></center>`);
+			newWin.$("body").append(`<center><div id="message"><p><b>${msg}</b></p></div><center>`);
+		};
+	} else {
+		alert('ГЌГҐГІ ГІГ ГЄГ®Г© Г§Г Г¤Г Г·ГЁ')
+	}
 };
 
 function searchTask(e){
-        e.preventDefault();
-		
-		let elements = new Array(...$("div[id^='task']"));
-		let pattern = prompt('Введите слово или номер задачи через #')
-	    if (pattern == ''){
-			elements.forEach(function (e){
+	e.preventDefault();
+	let elements = new Array(...$("div[id^='task']"));
+	let pattern = prompt('Г‚ГўГҐГ¤ГЁГІГҐ Г±Г«Г®ГўГ® ГЁГ«ГЁ Г­Г®Г¬ГҐГ° Г§Г Г¤Г Г·ГЁ Г·ГҐГ°ГҐГ§ #')
+	if (pattern == ''){
+		elements.forEach(function (e){
+			$(e).show();
+		});
+	} else {
+		let reg = new RegExp(pattern.toLowerCase());
+		elements.forEach(function (e){
+			if (!reg.test(e.innerHTML.toLowerCase())){
+				$(e).hide();
+			} else {
 				$(e).show();
-			});
-		} else {
-			//console.log(pattern);
-			let reg = new RegExp(pattern.toLowerCase());
-			elements.forEach(function (e){
-				if (!reg.test(e.innerHTML.toLowerCase())){
-					$(e).hide();
-				} else {
-					$(e).show();
-				}
-			});
-		}
+			}
+		});
+	}
 };
 	
 window.setInterval(getInfo, 10);
 window.setInterval(getWeekNumber, 10);
 
 $(document).ready(function(){	
-getInfo();
-getWeekNumber();
-loadChromeStorage();
-  
-$("body").on('click', "div[id^='task']", copyOnBuffer);
-$('#btn_add').click(createTask);
-$('#btn_remove').click(removeTask);
-$('#btn_clear').click(clearTaskSheldue);	
-$('#btn_timer').click(setTimerOnTask);
-$('#btn_search').click(searchTask);
+	getInfo();
+	getWeekNumber();
+	loadChromeStorage();
+	
+	$("body").on('click', "div[id^='task']", copyOnBuffer);
+	$('#btn_add').click(createTask);
+	$('#btn_remove').click(removeTask);
+	$('#btn_clear').click(clearTaskSheldue);	
+	$('#btn_timer').click(setTimerOnTask);
+	$('#btn_search').click(searchTask);
 });
-
-
-
-
-
